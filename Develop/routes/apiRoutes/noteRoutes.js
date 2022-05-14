0c3400e1902e2d71500c6eea414f3e780ced7766
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-13 21:58:25 -0500

    added get and pull requests to the to noteRoutes.js file and requiered the functions from the notes.js file to accomplish this. modified the html routes file to use the correct directory.

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
 const notes = require('../../db/db.json');
+const {
+    createNewNote,
+    deleteNote
+} = require ('../../lib/notes');
 
 
 router.get('/notes', (req, res) => {
-    console.log(notes)
+    
+    console.log(notes, 'get request');
     res.json(notes)
 });
+
+router.post('/notes', (req, res) => {
+    // console.log(req.body);
+    let noteInput = req.body;
+    let noteArr = notes;
+    // console.log(req.body, 'req body');
+
+     const newNote = createNewNote(noteInput, noteArr);
+
+    res.json(newNote);
+
+});
+
+// router.delete('/notes:id', (req, res) => {
+//     let noteId = req.params.id;
+//     let noteArray = notes;
+//      deleteNote(noteId, noteArray)
+//      res.json ()
+// })
+
  module.exports = router;
